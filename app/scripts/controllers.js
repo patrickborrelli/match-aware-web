4cb33ca7b84a2987abbfd5e4a82f7173848925af
Patrick Borrelli <patrickborrelli@gmail.com>
2017-02-14 20:25:44 -0500

    missed one

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -520,20 +520,7 @@ angular.module('ma-app')
                 .then(function(response) {
                     console.log("Successfully added age group: ");
                     console.log(response);
-                    coreDataService.refreshAgeGroups()
-                        .then(function(response) {
-                            coreDataService.setAgeGroups(response.data);
-                            $timeout(function(){
-                                $scope.ageGroups = coreDataService.getAgeGroups();
-                            })
-                            console.log("\n\n\nretrieved age groups and put them in scope");
-                            console.log($scope.ageGroups);
-                            ngDialog.close();
-                        }); 
-                }, function(errResponse) {
-                    console.log("Failed on attempt to add age group:");
-                    console.log(errResponse);
-                });   
+                     
         };
         
         $scope.openEditAgeGroup = function(ageGroup) {
